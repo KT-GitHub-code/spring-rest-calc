776fa574fd68d3a011fcd66532df12428e6397d9
KT <71466471+KT-GitHub-code@users.noreply.github.com>
2021-11-23 14:02:58 +0100

    Add operation routes

--- src/main/java/com/restcalc/restcalc/controller/CalcController.java
+++ src/main/java/com/restcalc/restcalc/controller/CalcController.java
@@ -1,6 +1,8 @@
 package com.restcalc.restcalc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,26 @@ public class CalcController {
     @GetMapping()
     public String index(){
         return "Welcome to the calculator app!";
+    }
+
+    @PostMapping("/add")
+    public int add(@RequestBody int[] nums){
+        return 0;
+    }
+
+    @PostMapping("/subtract")
+    public int subtract(@RequestBody int[] nums){
+        return 0;
+    }
+
+    @PostMapping("/multiply")
+    public int multiply(@RequestBody int[] nums){
+        return 0;
+    }
+
+    @PostMapping("/divide")
+    public double divide(@RequestBody int[] nums){
+        return 0.0;
     }
 
 }
