b2c4136d1b7f50a27839c257a65b2f344fb686e6
KT <71466471+KT-GitHub-code@users.noreply.github.com>
2021-11-23 13:14:27 +0100

    Initial commit

---
+++ src/test/java/com/restcalc/restcalc/RestcalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restcalc.restcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
